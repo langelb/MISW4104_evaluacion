81f0f42219d89fc05a1263abac41602d690f87c6
Luis Daniel Angel <l.angelb@uniandes.edu.co>
2023-04-29 14:01:11 -0500

    Solución punto 2

--- src/app/planta/planta-list/planta-list.component.ts
+++ src/app/planta/planta-list/planta-list.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { Planta } from '../planta';
 import { PlantaService } from '../planta.service';
+import { ChangeDetectionStrategy } from '@angular/compiler';
 
 @Component({
   selector: 'app-planta-list',
   templateUrl: './planta-list.component.html',
   styleUrls: ['./planta-list.component.css']
 })
-export class PlantaListComponent implements OnInit {
+export class PlantaListComponent {
 
   plantas: Array<Planta> = [];
+  counterInterior: number = 0;
+  counterExterior: number = 0;
 
   constructor(private plantaService: PlantaService) { }
 
   getPlantas(): void {
-   this.plantaService.getPlantas().subscribe((plantas) => {
-     this.plantas = plantas;
-   });
- }
+    this.plantaService.getPlantas().subscribe((plantas) => {
+      this.plantas = plantas;
+
+      for (let prop of this.plantas) {
+        if (prop.tipo == 'Interior') {
+          this.counterInterior++;
+        }
+        if (prop.tipo == 'Exterior') {
+          this.counterExterior++;
+        }
+      }
+    });
+  }
 
   ngOnInit() {
     this.getPlantas();
   }
-
 }
